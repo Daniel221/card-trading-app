066db541b721a31265e76f9e2d023e54ca6daf1f
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2021-04-05 21:05:50 -0500

    login

---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const usersController = require('../controllers/usersController');
+const users = new usersController();
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+//const passport = require('passport');
+
+router.post('/', async (req, res)=>{
+    let userData = req.body;
+    let foundUser = await users.getUserByEmail(userData.email);
+    if(!foundUser){
+        res.status(401).send('Email no encontrado')
+    }else
+    if(foundUser.password !== userData.password){
+        res.status(401).send('Contraseña incorrecta')
+    }
+    let payload = { subject: foundUser.id};
+    let token = jwt.sign(payload, 'secretKey')
+    res.status(200).send({token})
+});
+
+/*router.get('/login', (_, res)=>{
+    res.render('login');
+});
+
+router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    function(req, res){
+        console.log(req.user);
+        res.redirect('/profile');
+    }
+); 
+
+router.get('/google/logout', (req, res)=>{
+    req.logout();
+    res.redirect('/');
+});*/
+
+module.exports = router;
